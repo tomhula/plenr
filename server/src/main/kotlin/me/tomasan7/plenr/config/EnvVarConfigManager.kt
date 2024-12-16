b774e9809f270a1a67b267c0cfed821b1221ad6c
Tomasan7 <tomashula06@gmail.com>
2024-12-16 16:39:25 +0100

    Add more env var overrides

--- server/src/main/kotlin/me/tomasan7/plenr/config/EnvVarConfigManager.kt
+++ server/src/main/kotlin/me/tomasan7/plenr/config/EnvVarConfigManager.kt
@@ -2,7 +2,7 @@ package me.tomasan7.plenr.config
 
 /**
  * A [ConfigManager] that reads environment variables to override the primary [ConfigManager].
- * Overrides the server host and port.
+ * Overrides server.host, server.port, database.url, database.user, database.password, smtp.host, smtp.port, smtp.username, smtp.password.
  */
 class EnvVarConfigManager(
     private val primaryConfigManager: ConfigManager
@@ -10,14 +10,38 @@ class EnvVarConfigManager(
 {
     override suspend fun getConfig(): Config
     {
-        val jsonConfig = primaryConfigManager.getConfig()
-        val jsonServerConfig = jsonConfig.server
-        val serverHost = System.getenv(EnvVars.SERVER_HOST) ?: jsonServerConfig.host
-        val serverPort = System.getenv(EnvVars.SERVER_PORT)?.toIntOrNull() ?: jsonServerConfig.port
-        val serverConfig = jsonServerConfig.copy(host = serverHost, port = serverPort)
-        val finalConfig = jsonConfig.copy(server = serverConfig)
-        return finalConfig
+        val primaryConfig = primaryConfigManager.getConfig()
+        val server = overrideServer(primaryConfig.server)
+        val database = overrideDatabase(primaryConfig.database)
+        val smtp = overrideSmtp(primaryConfig.smtp)
+        return primaryConfig.copy(server = server, database = database, smtp = smtp)
     }
+
+    private fun overrideServer(primary: Config.Server): Config.Server
+    {
+        val host = EnvVars.SERVER_HOST.get() ?: primary.host
+        val port = EnvVars.SERVER_PORT.get()?.toIntOrNull() ?: primary.port
+        return primary.copy(host = host, port = port)
+    }
+
+    private fun overrideDatabase(primary: Config.Database): Config.Database
+    {
+        val url = EnvVars.DATABASE_URL.get() ?: primary.url
+        val user = EnvVars.DATABASE_USER.get() ?: primary.user
+        val password = EnvVars.DATABASE_PASSWORD.get() ?: primary.password
+        return primary.copy(url = url, user = user, password = password)
+    }
+
+    private fun overrideSmtp(primary: Config.Smtp): Config.Smtp
+    {
+        val host = EnvVars.SMTP_HOST.get() ?: primary.host
+        val port = EnvVars.SMTP_PORT.get()?.toIntOrNull() ?: primary.port
+        val username = EnvVars.SMTP_USERNAME.get() ?: primary.username
+        val password = EnvVars.SMTP_PASSWORD.get() ?: primary.password
+        return primary.copy(host = host, port = port, username = username, password = password)
+    }
+
+    private fun String.get() = System.getenv(this)
 
     private companion object
     {
@@ -25,6 +49,15 @@ class EnvVarConfigManager(
         {
             const val SERVER_HOST = "PLENR_SERVER_HOST"
             const val SERVER_PORT = "PLENR_SERVER_PORT"
+
+            const val DATABASE_URL = "PLENR_DATABASE_URL"
+            const val DATABASE_USER = "PLENR_DATABASE_USER"
+            const val DATABASE_PASSWORD = "PLENR_DATABASE_PASSWORD"
+
+            const val SMTP_HOST = "PLENR_SMTP_HOST"
+            const val SMTP_PORT = "PLENR_SMTP_PORT"
+            const val SMTP_USERNAME = "PLENR_SMTP_USERNAME"
+            const val SMTP_PASSWORD = "PLENR_SMTP_PASSWORD"
         }
     }
 }
