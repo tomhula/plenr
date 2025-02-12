e24af355f34c4babb18c68f9645c04a07965b9bd
Tomas Hula <tomashula06@gmail.com>
2025-02-12 18:11:11 +0100

    Training calendar WIP

--- shared/src/commonMain/kotlin/cz/tomashula/plenr/util/Week.kt
+++ shared/src/commonMain/kotlin/cz/tomashula/plenr/util/Week.kt
@@ -3,14 +3,16 @@ package cz.tomashula.plenr.util
 import kotlinx.datetime.*
 import kotlinx.serialization.Serializable
 import cz.tomashula.plenr.serialization.WeekSerializer
+import kotlinx.datetime.format.DateTimeFormat
 
 @Serializable(with = WeekSerializer::class)
-class Week(date: LocalDate)
+class Week(date: LocalDate): Comparable<Week>
 {
     val mondayDate = date.minus(date.dayOfWeek.ordinal, DateTimeUnit.DAY)
 
-    val dateRange = mondayDate..mondayDate.plus(7, DateTimeUnit.DAY)
-    val period = LocalDateTimePeriod(dateRange.start.atTime(LocalTime(0, 0)), dateRange.endInclusive.atTime(LocalTime(23, 59, 59)))
+    val dateRange = mondayDate..mondayDate.plus(6, DateTimeUnit.DAY)
+    val dateTimeRange = mondayDate.atTime(LocalTime(0, 0))..mondayDate.plus(7, DateTimeUnit.DAY).atTime(LocalTime(23, 59, 59))
+    val period = LocalDateTimePeriod(dateTimeRange.start, dateTimeRange.endInclusive)
     val days by lazy {
         (0 until 7).map { mondayDate.plus(it, DateTimeUnit.DAY) }
     }
@@ -18,6 +20,25 @@ class Week(date: LocalDate)
     operator fun contains(date: LocalDate) = date in dateRange
 
     operator fun contains(dateTime: LocalDateTime) = dateTime.date in this
+
+    override fun compareTo(other: Week): Int
+    {
+        return mondayDate.compareTo(other.mondayDate)
+    }
+
+    fun relative(relative: Int): Week
+    {
+        return Week(mondayDate.plus(relative * 7, DateTimeUnit.DAY))
+    }
+
+    fun differenceInWeeks(other: Week): Int
+    {
+        return (mondayDate.daysUntil(other.mondayDate) / 7).toInt()
+    }
+
+    override fun toString() = "${dateRange.start} - ${dateRange.endInclusive}"
+
+    fun toString(format: DateTimeFormat<LocalDate>) = "${dateRange.start.format(format)} - ${dateRange.endInclusive.format(format)}"
 
     companion object
     {
