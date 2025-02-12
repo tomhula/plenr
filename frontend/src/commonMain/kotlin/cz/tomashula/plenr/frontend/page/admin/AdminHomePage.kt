70a0441fb939988cba13f17a2d204b54805aeb85
Tomas Hula <tomashula06@gmail.com>
2025-02-12 18:16:05 +0100

    Update admin home page

--- frontend/src/commonMain/kotlin/cz/tomashula/plenr/frontend/page/admin/AdminHomePage.kt
+++ frontend/src/commonMain/kotlin/cz/tomashula/plenr/frontend/page/admin/AdminHomePage.kt
@@ -9,23 +9,29 @@ import dev.kilua.html.h2t
 import dev.kilua.html.px
 import cz.tomashula.plenr.frontend.MainViewModel
 import cz.tomashula.plenr.frontend.component.applyColumn
+import dev.kilua.compose.foundation.layout.Arrangement
+import dev.kilua.compose.foundation.layout.Column
+import dev.kilua.compose.foundation.layout.Row
+import dev.kilua.html.bsButton
 
 @Composable
 fun IComponent.adminHomePage(viewModel: MainViewModel)
 {
     val router = Router.current
 
-    div {
-        applyColumn()
-        rowGap(10.px)
-        button("Manage users", className = "primary-button") {
-            onClick {
-                router.navigate("/manage-users")
+    Column {
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(16.px),
+        ) {
+            bsButton("Manage users") {
+                onClick {
+                    router.navigate("/manage-users")
+                }
             }
-        }
-        button("Arrange trainings", className = "primary-button") {
-            onClick {
-                router.navigate("/arrange-trainings")
+            bsButton("Arrange trainings") {
+                onClick {
+                    router.navigate("/arrange-trainings")
+                }
             }
         }
         h2t("This week:")
