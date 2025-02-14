a6ddb9cc1e68db1e9091d9d50a9320ccae1b623c
Tomas Hula <tomashula06@gmail.com>
2025-02-14 20:39:02 +0100

    Remove left behind println

--- frontend/src/commonMain/kotlin/cz/tomashula/plenr/frontend/page/login/LoginPage.kt
+++ frontend/src/commonMain/kotlin/cz/tomashula/plenr/frontend/page/login/LoginPage.kt
@@ -27,7 +27,6 @@ fun IComponent.loginPage(mainViewModel: MainViewModel)
         bsForm<LoginForm>(
             onSubmit = { data, _, _ ->
                 authenticated = mainViewModel.login(data.email!!, data.password!!)
-                println("HO")
                 if (authenticated == true)
                     router.navigate(Route.HOME)
             }
