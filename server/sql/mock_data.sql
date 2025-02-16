8fb2d5bb80591459d299a23f339f9d5f75f1b9cb
Tomas Hula <tomashula06@gmail.com>
2025-02-16 09:42:24 +0100

    Update mock data

--- server/sql/mock_data.sql
+++ server/sql/mock_data.sql
@@ -1,5 +1,6 @@
 INSERT INTO user (first_name, last_name, email, phone, password_hash, is_admin) VALUES
-                                                                                    ('John', 'Doe', 'john.doe@example.com', '+1234567890', 0x5E884898DA28047151D0E56F8DC6292773603D0D6AABBDD62A11EF721D1542D8, 1),
+                                                                                    ('Tomáš', 'Hůla', 'tomashula@example.com', '+420 123 456 789', 0x5E884898DA28047151D0E56F8DC6292773603D0D6AABBDD62A11EF721D1542D8, 1),
+                                                                                    ('John', 'Doe', 'john.doe@example.com', '+1234567890', 0x5E884898DA28047151D0E56F8DC6292773603D0D6AABBDD62A11EF721D1542D8, 0),
                                                                                     ('Jane', 'Smith', 'jane.smith@example.com', '+1234567891', 0x5E884898DA28047151D0E56F8DC6292773603D0D6AABBDD62A11EF721D1542D8, 0),
                                                                                     ('Alice', 'Johnson', 'alice.johnson@example.com', '+1234567892', 0x5E884898DA28047151D0E56F8DC6292773603D0D6AABBDD62A11EF721D1542D8, 0),
                                                                                     ('Bob', 'Brown', 'bob.brown@example.com', '+1234567893', 0x5E884898DA28047151D0E56F8DC6292773603D0D6AABBDD62A11EF721D1542D8, 0),
@@ -12,25 +13,25 @@ INSERT INTO user (first_name, last_name, email, phone, password_hash, is_admin) 
 
 INSERT INTO training (arranger_id, name, description, type, start_date_time, length_minutes) VALUES
                                                                                                  (1, 'Yoga Basics', 'Introduction to yoga.', 1, '2025-02-14 08:00:00.000000', 60),
-                                                                                                 (2, 'Advanced Pilates', 'High-intensity pilates.', 0, '2025-02-15 10:00:00.000000', 75),
-                                                                                                 (3, 'Strength Training', 'Full-body strength workout.', 1, '2025-02-16 09:00:00.000000', 90),
-                                                                                                 (4, 'Cardio Blast', 'Intense cardio session.', 0, '2025-02-17 07:00:00.000000', 45),
-                                                                                                 (5, 'HIIT Workout', 'High-intensity interval training.', 1, '2025-02-18 18:00:00.000000', 30),
-                                                                                                 (6, 'CrossFit Challenge', 'Advanced crossfit workout.', 0, '2025-02-19 12:00:00.000000', 60),
-                                                                                                 (7, 'Spin Class', 'High-energy spin session.', 1, '2025-02-20 17:00:00.000000', 45),
-                                                                                                 (8, 'Functional Fitness', 'Practical strength and agility.', 0, '2025-02-21 14:00:00.000000', 60),
-                                                                                                 (9, 'Zumba Dance', 'Dance your way to fitness.', 1, '2025-02-22 11:00:00.000000', 60),
-                                                                                                 (10, 'Bootcamp', 'Military-style training.', 0, '2025-02-23 06:00:00.000000', 60),
+                                                                                                 (1, 'Advanced Pilates', 'High-intensity pilates.', 0, '2025-02-15 10:00:00.000000', 75),
+                                                                                                 (1, 'Strength Training', 'Full-body strength workout.', 1, '2025-02-16 09:00:00.000000', 90),
+                                                                                                 (1, 'Cardio Blast', 'Intense cardio session.', 0, '2025-02-17 07:00:00.000000', 45),
+                                                                                                 (1, 'HIIT Workout', 'High-intensity interval training.', 1, '2025-02-18 18:00:00.000000', 30),
+                                                                                                 (1, 'CrossFit Challenge', 'Advanced crossfit workout.', 0, '2025-02-19 12:00:00.000000', 60),
+                                                                                                 (1, 'Spin Class', 'High-energy spin session.', 1, '2025-02-20 17:00:00.000000', 45),
+                                                                                                 (1, 'Functional Fitness', 'Practical strength and agility.', 0, '2025-02-21 14:00:00.000000', 60),
+                                                                                                 (1, 'Zumba Dance', 'Dance your way to fitness.', 1, '2025-02-22 11:00:00.000000', 60),
+                                                                                                 (1, 'Bootcamp', 'Military-style training.', 0, '2025-02-23 06:00:00.000000', 60),
                                                                                                  (1, 'Core Strength', 'Core and stability workout.', 1, '2025-02-24 09:30:00.000000', 45),
-                                                                                                 (2, 'Stretch and Relax', 'Flexibility and relaxation.', 0, '2025-02-25 19:00:00.000000', 60),
-                                                                                                 (3, 'Kickboxing Basics', 'Martial arts-inspired workout.', 1, '2025-02-26 18:00:00.000000', 60),
-                                                                                                 (4, 'Aqua Aerobics', 'Low-impact water workout.', 0, '2025-02-27 10:00:00.000000', 45),
-                                                                                                 (5, 'Trail Running', 'Outdoor endurance run.', 1, '2025-02-28 08:00:00.000000', 120),
-                                                                                                 (6, 'Dance Cardio', 'Energetic dance workout.', 0, '2025-03-01 16:00:00.000000', 45);
+                                                                                                 (1, 'Stretch and Relax', 'Flexibility and relaxation.', 0, '2025-02-25 19:00:00.000000', 60),
+                                                                                                 (1, 'Kickboxing Basics', 'Martial arts-inspired workout.', 1, '2025-02-26 18:00:00.000000', 60),
+                                                                                                 (1, 'Aqua Aerobics', 'Low-impact water workout.', 0, '2025-02-27 10:00:00.000000', 45),
+                                                                                                 (1, 'Trail Running', 'Outdoor endurance run.', 1, '2025-02-28 08:00:00.000000', 120),
+                                                                                                 (1, 'Dance Cardio', 'Energetic dance workout.', 0, '2025-03-01 16:00:00.000000', 45);
 
 
 INSERT INTO training_participant (training_id, participant_id) VALUES
                                                                    (1, 2), (1, 3), (2, 4), (2, 5), (3, 1), (3, 6), (4, 7), (4, 8), (5, 9), (5, 10),
                                                                    (6, 2), (6, 3), (7, 4), (7, 5), (8, 6), (8, 7), (9, 8), (9, 9), (10, 10), (10, 1),
                                                                    (11, 3), (11, 4), (12, 5), (12, 6), (13, 7), (13, 8), (14, 9), (14, 10), (15, 1), (15, 2),
-                                                                   (16, 3), (16, 4), (17, 5), (17, 6), (18, 7), (18, 8), (19, 9), (19, 10), (20, 1), (20, 2);
+                                                                   (16, 3), (16, 4);
