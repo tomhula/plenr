af720e46fe2d8f95cbce3918f9f61b295e6084a3
Tomas Hula <tomashula06@gmail.com>
2025-03-06 20:47:46 +0100

    Remove unused code

--- frontend/src/commonMain/kotlin/cz/tomashula/plenr/frontend/page/UserHomePage.kt
+++ frontend/src/commonMain/kotlin/cz/tomashula/plenr/frontend/page/UserHomePage.kt
@@ -3,23 +3,14 @@ package cz.tomashula.plenr.frontend.page
 import androidx.compose.runtime.*
 import app.softwork.routingcompose.Router
 import cz.tomashula.plenr.feature.training.TrainingWithParticipantsDto
-import cz.tomashula.plenr.feature.user.UserDto
 import cz.tomashula.plenr.frontend.MainViewModel
 import cz.tomashula.plenr.frontend.component.bsObjectDialog
-import cz.tomashula.plenr.frontend.component.participantBadge
 import cz.tomashula.plenr.frontend.component.rememberObjectDialogState
 import cz.tomashula.plenr.frontend.component.trainingCalendar
 import cz.tomashula.plenr.frontend.component.trainingDialogBody
 import cz.tomashula.plenr.util.Week
 import dev.kilua.core.IComponent
-import dev.kilua.html.bt
-import dev.kilua.html.div
-import dev.kilua.html.px
-import dev.kilua.html.spant
-import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.atTime
-import kotlinx.datetime.format
-import kotlinx.datetime.format.char
 
 @Composable
 fun IComponent.userHomePage(viewModel: MainViewModel)
@@ -49,15 +40,4 @@ fun IComponent.userHomePage(viewModel: MainViewModel)
         trainings = arrangedTrainings,
         onTrainingClick = { trainingDialogState.show(it) }
     )
-}
-
-
-private val dateTimeFormat = LocalDateTime.Format {
-    dayOfMonth()
-    char('.')
-    monthNumber()
-    chars(". ")
-    hour()
-    char(':')
-    minute()
 }
