c4bf9266198bc76c340d5ec58dd1e914df471d96
Tomas Hula <tomashula06@gmail.com>
2025-06-07 18:01:09 +0200

    Add check for training length to be positive

--- server/src/main/kotlin/cz/tomashula/plenr/feature/training/TrainingTable.kt
+++ server/src/main/kotlin/cz/tomashula/plenr/feature/training/TrainingTable.kt
@@ -11,6 +11,6 @@ object TrainingTable : IntIdTable("training")
     val description = varchar("description", 255)
     val type = enumeration<TrainingType>("type")
     val startDateTime = datetime("start_date_time")
-    val lengthMinutes = integer("length_minutes")
+    val lengthMinutes = integer("length_minutes").check { it greater 0 }
     val cancelled = bool("cancelled").default(false)
 }
