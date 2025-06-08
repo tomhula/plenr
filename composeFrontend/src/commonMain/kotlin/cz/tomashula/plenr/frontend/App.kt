0e01a4527d0f09288c9ff4b223dbe05b91a1ab89
Tomas Hula <tomashula06@gmail.com>
2025-06-08 14:06:33 +0200

    Add header padding

--- composeFrontend/src/commonMain/kotlin/cz/tomashula/plenr/frontend/App.kt
+++ composeFrontend/src/commonMain/kotlin/cz/tomashula/plenr/frontend/App.kt
@@ -3,11 +3,13 @@ package cz.tomashula.plenr.frontend
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
@@ -75,7 +77,9 @@ fun App(
                         popUpTo(PlenrScreen.Login.name) { inclusive = true }
                     }
                 },
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 16.dp)
             )
             
             NavHost(
