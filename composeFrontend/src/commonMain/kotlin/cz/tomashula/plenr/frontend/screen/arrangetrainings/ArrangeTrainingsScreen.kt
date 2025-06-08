67cfe9a13c69a8f35a1014744350283ffea3dc51
Tomas Hula <tomashula06@gmail.com>
2025-06-08 19:40:48 +0200

    Improve user availability look

--- composeFrontend/src/commonMain/kotlin/cz/tomashula/plenr/frontend/screen/arrangetrainings/ArrangeTrainingsScreen.kt
+++ composeFrontend/src/commonMain/kotlin/cz/tomashula/plenr/frontend/screen/arrangetrainings/ArrangeTrainingsScreen.kt
@@ -9,18 +9,19 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberBasicTooltipState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.text.drawText
-import androidx.compose.ui.text.font.FontVariation.width
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.unit.IntSize
@@ -544,6 +545,7 @@ private fun UserAvailabilityPart(
             .fillMaxHeight()
             .width((durationMinutes / 60f * hourWidth).dp)
             .offset(x = ((startMinute / 60f - startHour) * hourWidth).dp)
+            .clip(RoundedCornerShape(50))
             .background(color)
     )
 }
