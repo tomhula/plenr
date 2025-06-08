b183e7c409b7f51e2741f00bc214245fcb465e0e
Tomas Hula <tomashula06@gmail.com>
2025-06-08 19:18:54 +0200

    Improve participant badge

--- composeFrontend/src/commonMain/kotlin/cz/tomashula/plenr/frontend/ui/component/ParticipantBadge.kt
+++ composeFrontend/src/commonMain/kotlin/cz/tomashula/plenr/frontend/ui/component/ParticipantBadge.kt
@@ -4,16 +4,15 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import cz.tomashula.plenr.feature.user.UserDto
 import cz.tomashula.plenr.frontend.ui.Colors
-import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 fun ParticipantBadge(
@@ -22,12 +21,13 @@ fun ParticipantBadge(
 ) {
     Box(
         modifier = modifier
-            .clip(RoundedCornerShape(4.dp))
+            .clip(RoundedCornerShape(8.dp))
             .background(Colors.getColor(participant.id))
             .padding(horizontal = 8.dp, vertical = 4.dp)
     ) {
         Text(
             text = participant.fullName,
+            style = MaterialTheme.typography.labelMedium,
             color = Color.Black
         )
     }
