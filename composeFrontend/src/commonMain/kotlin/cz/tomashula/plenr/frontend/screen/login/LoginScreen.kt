3549af7d796944c772f9c9587371683de4f547b1
Tomas Hula <tomashula06@gmail.com>
2025-06-08 16:55:37 +0200

    Login on enter in password field

--- composeFrontend/src/commonMain/kotlin/cz/tomashula/plenr/frontend/screen/login/LoginScreen.kt
+++ composeFrontend/src/commonMain/kotlin/cz/tomashula/plenr/frontend/screen/login/LoginScreen.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
@@ -13,6 +15,7 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -49,6 +52,14 @@ fun LoginScreen(
             onValueChange = viewModel::onPasswordChange,
             label = { Text("Password") },
             singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Done,
+            ),
+            keyboardActions = KeyboardActions(
+                onDone = {
+                    viewModel.login(onSuccess = onLogin)
+                }
+            ),
             visualTransformation = PasswordVisualTransformation(),
             modifier = Modifier.fillMaxWidth()
         )
