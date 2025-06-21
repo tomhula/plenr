eeb92c0522f1dcc41adc63f793eef379ebaa0b5f
Tomas Hula <tomashula06@gmail.com>
2025-06-21 19:14:42 +0200

    Set readable URLs to navigation destinations

--- composeFrontend/src/commonMain/kotlin/cz/tomashula/plenr/frontend/PlenrScreen.kt
+++ composeFrontend/src/commonMain/kotlin/cz/tomashula/plenr/frontend/PlenrScreen.kt
@@ -1,5 +1,6 @@
 package cz.tomashula.plenr.frontend
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 sealed class PlenrScreen
@@ -8,21 +9,30 @@ sealed class PlenrScreen
         get() = this::class.simpleName!! // TODO: Replace with StringResource for each destination
     
     @Serializable
+    @SerialName("admin-setup")
     data object AdminSetup : PlenrScreen()
     @Serializable
+    @SerialName("set-password")
     data class SetPassword(val token: String) : PlenrScreen()
     @Serializable
+    @SerialName("login")
     data object Login : PlenrScreen()
     @Serializable
+    @SerialName("home")
     data object Home : PlenrScreen()
     @Serializable
+    @SerialName("availability")
     data object Availability : PlenrScreen()
     @Serializable
+    @SerialName("preferences")
     data object Preferences : PlenrScreen()
     @Serializable
+    @SerialName("arrange-trainings")
     data object ArrangeTrainings : PlenrScreen()
     @Serializable
+    @SerialName("manage-users")
     data object ManageUsers : PlenrScreen()
     @Serializable
+    @SerialName("forgot-password")
     data object ForgotPassword : PlenrScreen()
 }
