64689a638c55ac2aee8a5adcf3901b590292d93c
Tomas Hula <tomashula06@gmail.com>
2025-06-21 20:03:38 +0200

    Fix set-password page not working properly

--- composeFrontend/src/wasmJsMain/kotlin/cz/tomashula/plenr/frontend/Main.kt
+++ composeFrontend/src/wasmJsMain/kotlin/cz/tomashula/plenr/frontend/Main.kt
@@ -4,6 +4,8 @@ import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
 import androidx.navigation.ExperimentalBrowserHistoryApi
 import androidx.navigation.bindToNavigation
+import androidx.navigation.decodeURIComponent
+import io.ktor.http.decodeURLPart
 import kotlinx.browser.document
 import kotlinx.browser.window
 
@@ -12,7 +14,21 @@ import kotlinx.browser.window
 fun main() {
     ComposeViewport(document.body!!) {
         App(
-            onNavHostReady = { window.bindToNavigation(it) }
+            onNavHostReady = { navController ->
+                val initRoute = window.location.hash.substringAfter('#', "")
+                when {
+                    initRoute.startsWith("set-password/") -> {
+                        val tokenEscaped = initRoute.removePrefix("set-password/")
+                        val token = tokenEscaped.decodeURLPart()
+                        println("TokenEscaped: $tokenEscaped, Token: $token")
+                        navController.navigate(PlenrScreen.SetPassword(token))
+                    }
+                    else -> {
+                        navController.navigate(initRoute)
+                    }
+                }
+                window.bindToNavigation(navController) 
+            }
         )
     }
 }
